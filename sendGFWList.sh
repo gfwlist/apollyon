ea46828a373801e207155e66de2fbb6ad7544fee
Christopher Meng <i@cicku.me>
2016-10-13 14:54:23 +0200

    Correct license

--- sendGFWList.sh
+++ sendGFWList.sh
@@ -2,8 +2,8 @@
 # (C) Copyright 2009-2016 GFWList
 # 
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Shell script to maintain GFWList.
